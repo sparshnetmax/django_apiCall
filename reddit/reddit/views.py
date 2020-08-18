bdd20f23b88677d6c65a0f1b198f713082fbcdc2
sparsh <devnet1985@gmail.com>
2020-08-18 15:05:06 +0530

    new changes working redirect

--- reddit/reddit/views.py
+++ reddit/reddit/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import redirect,render,HttpResponse
+from django.http.response import JsonResponse
+
 import json
 from reddit.reditCreds import refToken
 def redirectUri(request):
     code = request.GET['code']
+    print(code)
     data = refToken(code)
-    return HttpResponse(json.dumps(data))
+    data=dict(data)
+    return JsonResponse(data)
 
 def index(request):
-
     return render(request, 'index.html')
